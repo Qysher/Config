ddc33a802774b2655786d4d85a4c72397edca82a
moritz <lilpalli59@gmail.com>
2020-06-25 11:46:09 +0200

    Class cast

--- src/qysher/configsystem/test/TestStart.java
+++ src/qysher/configsystem/test/TestStart.java
@@ -3,16 +3,14 @@ package qysher.configsystem.test;
 import qysher.configsystem.core.ConfigSystem;
 
 import java.io.File;
-import java.util.Arrays;
 
 public class TestStart {
 
     public static void main(String[] args) {
-        ConfigSystem configSystem = new ConfigSystem(new File("config.cfg"));
-        configSystem.load();
-        //configSystem.set("Hallo", "Test");
-        configSystem.save();
-        System.out.println(Arrays.toString(configSystem.values.entrySet().toArray()));
+        ConfigSystem configSystem = new ConfigSystem(new File("config.cfg")).load().registerShutdownHook();
+        configSystem.set("Hallo", "Test");
+        String str = configSystem.get("Hallo");
+        System.out.println(configSystem.getAs("Hallo", String.class));
     }
 
 }
